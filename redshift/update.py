5e936ec6217123ec012ebf2ef59a826f781941da
Adam Dobrawy <adam@getselectstar.com>
2022-06-04 05:23:07 +0200

    redshift: add permission to each database granted

--- redshift/update.py
+++ redshift/update.py
@@ -1,7 +1,15 @@
 import json
+import sys
+
+BUCKET = sys.argv[1]
+KEY = sys.argv[2]
 
 with open('SelectStarRedshift.json', 'r') as f:
     data = json.load(f)
-with open('SelectStarRedshift.json', 'w') as f, open('provision.py', 'r') as script_fp:
-    data['Resources']['LambdaFunction']['Properties']['Code']['ZipFile'] = script_fp.read()
+with open('SelectStarRedshift.json', 'w') as f:
+    for key in ["LambdaProvisionFunction", 'LambdaTransformFunction']:
+        data["Resources"][key]["Properties"]["Code"] = {
+            "S3Bucket": BUCKET,
+            "S3Key": KEY,
+        }
     json.dump(data, f, indent=4,)
