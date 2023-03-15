a20baf974267641e58e9658543718ce61887222c
Adam Dobrawy <adam@getselectstar.com>
2023-03-15 02:30:15 +0100

    Restrict 's3:GetObject' permissin for Redshift [sc-47467]

--- redshift/update.py
+++ redshift/update.py
@@ -12,6 +12,9 @@ with open("SelectStarRedshift.json", "w") as f, open("copy_handler.py", "r") as 
     }
     data["Resources"]["LambdaCopy"]["Properties"]["srcBucket"] = BUCKET
     data["Resources"]["LambdaCopy"]["Properties"]["srcKey"] = KEY
+    data["Resources"]["LambdaRolePolicy"]["Properties"]["PolicyDocument"]["Statement"][
+        0
+    ]["Resource"] = f"arn:aws:s3:::{BUCKET}/{KEY}"
     json.dump(
         data,
         f,
