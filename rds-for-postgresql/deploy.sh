209e2bddcda5cf00f6d99a7f053acab37b905c60
Devendra Gera <gera@getselectstar.com>
2023-03-31 19:00:03 +0200

    fix provisioning script

--- rds-for-postgresql/deploy.sh
+++ rds-for-postgresql/deploy.sh
@@ -5,14 +5,14 @@ PREFIX="${2-rds-for-postgresql}"
 
 # Build deployment package
 rm -f deployment-package.zip
-pip install --target ./package -r requirements.txt
+pip3 install --target ./package -r requirements.txt
 pushd package
 zip -r ../deployment-package.zip .
 zip -g ../deployment-package.zip ../provision.py
 zip -g ../deployment-package.zip ../cfnresponse.py
 popd;
 # Generate templates
-python update.py "$BUCKET" "$PREFIX/deployment-package.zip"
+python3 update.py "$BUCKET" "$PREFIX/deployment-package.zip"
 # Upload files
 sha512sum ./SelectStarRDS.json ./deployment-package.zip
 aws s3 cp ./SelectStarRDS.json "s3://$BUCKET/$PREFIX/" --acl public-read
