c92b8071882ddd446fc50b679a3fe9a1bcc071b7
Adam Dobrawy <adam@getselectstar.com>
2023-04-20 01:11:52 +0200

    add e2e tests & terraform module for aws glue [sc-44464]

---
+++ glue/terraform/main.tf
@@ -0,0 +1,22 @@
+resource "random_id" "master-identifier" {
+  keepers = {
+    prefix       = var.name_prefix
+    ExternalId   = var.external_id
+    IamPrincipal = var.iam_principal
+  }
+  byte_length = 8
+}
+
+resource "aws_cloudformation_stack" "stack-master" {
+  name = "${var.name_prefix}-${random_id.master-identifier.hex}"
+
+  disable_rollback = true
+
+  parameters = {
+    ExternalId   = var.external_id
+    IamPrincipal = var.iam_principal
+  }
+
+  capabilities = ["CAPABILITY_IAM"]
+  template_url = var.template_url
+}
