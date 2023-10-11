a87209bf98e62f25afd7d88cd5762523d79165b1
Adam Dobrawy <adam@getselectstar.com>
2023-10-11 03:17:25 +0200

    fix: 🐛 minor cleanup [sc-64021]

--- rds-for-postgresql/deploy.sh
+++ rds-for-postgresql/deploy.sh
@@ -3,17 +3,6 @@ set -eux
 BUCKET="${1-cf-templates-pp3cips1o7jf-us-east-2}"
 PREFIX="${2-rds-for-postgresql}"
 
-# Build deployment package
-rm -f deployment-package.zip
-pip3 install --target ./package -r requirements.txt
-pushd package
-zip -r ../deployment-package.zip .
-zip -g ../deployment-package.zip ../provision.py
-zip -g ../deployment-package.zip ../cfnresponse.py
-popd;
-# Generate templates
-python3 update.py "$BUCKET" "$PREFIX/deployment-package.zip"
 # Upload files
-sha512sum ./SelectStarRDS.json ./deployment-package.zip
+sha512sum ./SelectStarRDS.json
 aws s3 cp ./SelectStarRDS.json "s3://$BUCKET/$PREFIX/" --acl public-read
-aws s3 cp ./deployment-package.zip "s3://$BUCKET/$PREFIX/" --acl public-read
