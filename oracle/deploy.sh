12ada8e6c237bc38c8ddc6e0ed12ce5021770098
Adam Dobrawy <adam@getselectstar.com>
2024-08-29 00:33:43 +0200

    fix: 🐛 typo [sc-91589]

--- oracle/deploy.sh
+++ oracle/deploy.sh
@@ -4,8 +4,8 @@ BUCKET="${1-cf-templates-pp3cips1o7jf-us-east-2}"
 PREFIX="${2-oracle}"
 
 # Convert YAML to JSON
-cat ./SelectStarRedshift.yaml |  yq e -j > ./SelectStarRedshift.json
+cat ./SelectStarOracle.yaml |  yq e -j > ./SelectStarOracle.json
 
 # Upload files
-aws s3 cp ./SelectStarRedshift.json "s3://$BUCKET/$PREFIX/" --acl public-read
+aws s3 cp ./SelectStarOracle.json "s3://$BUCKET/$PREFIX/" --acl public-read
 aws s3 cp ./deployment-package.zip "s3://$BUCKET/$PREFIX/" --acl public-read
